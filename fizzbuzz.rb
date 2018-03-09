a5720e1316de525bfc51d5a31d3f59321cdc4e25
Precious Humphrey <phumphrey23@gmail.com>
2018-03-09 01:25:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number % 3 == 0 && number % 5 == 0
     "Fizz"
   elsif number % 5 == 0
     "Buzz"
-  elsif number % 3 == 0 && (number % 5 == 0)
+  elsif number % 3 == 0
     "FizzBuzz"
   else number
   end
