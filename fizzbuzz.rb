4c4b3433fde9d21fbce7b7956810e4c72fc1c8e1
Precious Humphrey <phumphrey23@gmail.com>
2018-03-09 01:30:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
+  if number % 3 == 0
     "Fizz"
   elsif number % 5 == 0
     "Buzz"
-  elsif number % 3 == 0
+  elsif number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
-  else number
+  else 
+    nil
   end
 end
